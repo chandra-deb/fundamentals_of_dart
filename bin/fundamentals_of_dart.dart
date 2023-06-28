382b79a2abdddcb28a1b664714f2793aa9258b14
chandra-deb <mail.chandradeb@gmail.com>
2023-06-28 13:42:56 +0600

    variables and primitive types

--- bin/fundamentals_of_dart.dart
+++ bin/fundamentals_of_dart.dart
@@ -1,3 +1,25 @@
 void main(List<String> arguments) {
-  print('Hello world');
+  String myString = "Hello World";
+  int myInt = 1;
+  double myDouble = 1.5;
+
+// num type is parent class of int and double
+  num myNum = 2;
+  myNum = 2.4;
+
+  bool myBool = true;
+  myBool = false;
+
+  dynamic mySometing = 5;
+  mySometing = 'Hello';
+  mySometing = false;
+  mySometing = null;
+
+  final String myFinal;
+  myFinal = 'wow';
+
+  var myType = 'World';
+  Object test;
+  test = 2;
+  test = 'Hello';
 }
