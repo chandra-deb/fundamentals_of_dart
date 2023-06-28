9c858e4d1ab6b2805601dcbe9dd121fd045fc886
chandra-deb <mail.chandradeb@gmail.com>
2023-06-28 14:07:10 +0600

    operators

--- bin/fundamentals_of_dart.dart
+++ bin/fundamentals_of_dart.dart
@@ -1,25 +1,21 @@
 void main(List<String> arguments) {
-  String myString = "Hello World";
-  int myInt = 1;
-  double myDouble = 1.5;
+  num x = 1;
+  x + 1;
+  x - 1;
+  x * 1;
+  x / 1;
+  x ~/ 1;
 
-// num type is parent class of int and double
-  num myNum = 2;
-  myNum = 2.4;
+  x += 1;
+  x -= 1;
+  x *= 1;
+  x /= 1;
+  x ~/= 1;
 
-  bool myBool = true;
-  myBool = false;
-
-  dynamic mySometing = 5;
-  mySometing = 'Hello';
-  mySometing = false;
-  mySometing = null;
-
-  final String myFinal;
-  myFinal = 'wow';
-
-  var myType = 'World';
-  Object test;
-  test = 2;
-  test = 'Hello';
+  bool isEqual = 5 == 10;
+  isEqual = 5 > 10;
+  isEqual = 5 < 10;
+  isEqual = 5 <= 10;
+  isEqual = 5 >= 10;
+  print('Hello $isEqual');
 }
